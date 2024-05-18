1e29a1fcb99f3095173067f179b8c1bc8dedc52d
FUGA\abdur <zidobefreelance@gmail.com>
2024-05-18 17:09:45 +0400

    added MonoGame

---
+++ ConsoleAdventure/Content/Scripts/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleAdventure.Settings;
+using System;
+
+namespace ConsoleAdventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            World.World world = new World.World();
+            string lastKey;
+
+            while (true)
+            {
+                Console.Write($"{Docs.GetInfo()}\n\n{world.Render()}");
+                lastKey = Console.ReadKey().ToString();
+                Console.Clear();
+            }
+        }
+    }
+}
--- ConsoleAdventure/Program.cs
+++ ConsoleAdventure/Program.cs
@@ -1,21 +1,3 @@
-﻿using ConsoleAdventure.Settings;
-using System;
-
-namespace ConsoleAdventure
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            World.World world = new World.World();
-            string lastKey;
-
-            while (true)
-            {
-                Console.Write($"{Docs.GetInfo()}\n\n{world.Render()}");
-                lastKey = Console.ReadKey().ToString();
-                Console.Clear();
-            }
-        }
-    }
-}
+﻿
+using var game = new ConsoleAdventure.Game1();
+game.Run();
