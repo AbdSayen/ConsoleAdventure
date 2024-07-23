481a08b979209ea15819d2731363266b20aedc40
Roman <alivad@list.ru>
2024-07-23 22:18:31 +0300

    [edit] keys naming, cursor logic

--- ConsoleAdventure/Content/Scripts/Player/Cursor.cs
+++ ConsoleAdventure/Content/Scripts/Player/Cursor.cs
@@ -5,27 +5,40 @@ namespace ConsoleAdventure.Content.Scripts.Player;
 public class Cursor
 {
     public Position CursorPosition;
+    public bool IsVisible { get; set; }
+    
+    public static Cursor Instance => _instance;
 
+    private static Cursor _instance;
+    
     public Cursor()
     {
         CursorPosition = Position.Zero();
+
+        if (_instance == null)
+        {
+            _instance = this;
+        }
     }
-    
+
     public void CursorMovement()
     {
-        if (Input.IsKeyDown(InputConfig.BuildingUp) && CursorPosition.y > -2)
+        if (Input.IsKeyDown(InputConfig.CursorUp) && CursorPosition.y > -2)
         {
             CursorPosition.SetPosition(CursorPosition.x, CursorPosition.y - 1);
         }
-        if (Input.IsKeyDown(InputConfig.BuildingDown) && CursorPosition.y < 2)
+
+        if (Input.IsKeyDown(InputConfig.CursorDown) && CursorPosition.y < 2)
         {
             CursorPosition.SetPosition(CursorPosition.x, CursorPosition.y + 1);
         }
-        if (Input.IsKeyDown(InputConfig.BuildingLeft) && CursorPosition.x > -2)
+
+        if (Input.IsKeyDown(InputConfig.CursorLeft) && CursorPosition.x > -2)
         {
             CursorPosition.SetPosition(CursorPosition.x - 1, CursorPosition.y);
         }
-        if (Input.IsKeyDown(InputConfig.BuildingRight) && CursorPosition.x < 2)
+
+        if (Input.IsKeyDown(InputConfig.CursorRight) && CursorPosition.x < 2)
         {
             CursorPosition.SetPosition(CursorPosition.x + 1, CursorPosition.y);
         }
