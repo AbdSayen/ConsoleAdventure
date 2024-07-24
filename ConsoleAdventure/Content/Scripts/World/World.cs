fbbe8076e6e17031c0fec74ff8d6f8f60d326471
ZaLa0987654321 <gl4444eb@gmail.com>
2024-07-24 23:21:40 +0300

    - Correct Rename Entities in World.cs -

--- ConsoleAdventure/Content/Scripts/World/World.cs
+++ ConsoleAdventure/Content/Scripts/World/World.cs
@@ -17,7 +17,7 @@ namespace ConsoleAdventure.WorldEngine
 
         public List<List<Chunk>> chunks = new List<List<Chunk>>();
         public List<Player> players = new List<Player>();
-        public List<Entity> entites = new List<Entity>();
+        public List<Entity> entities = new List<Entity>();
 
         public Time time = new Time();
 
@@ -55,7 +55,7 @@ namespace ConsoleAdventure.WorldEngine
             {
                 for (int j = 0; j < 15; j++)
                 {
-                    entites.Add(new Cat(this, new(6 + i, 6 + j)));
+                    entities.Add(new Cat(this, new(6 + i, 6 + j)));
                 }
             }
 
@@ -84,9 +84,9 @@ namespace ConsoleAdventure.WorldEngine
                     players[i].InteractWithWorld();
                 }
 
-                for (int i = 0; i < entites.Count; i++)
+                for (int i = 0; i < entities.Count; i++)
                 {
-                    entites[i].InteractWithWorld();
+                    entities[i].InteractWithWorld();
                 }
             }
 
