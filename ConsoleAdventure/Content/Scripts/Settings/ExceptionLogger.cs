981f750aee3d9f79f3e5ee9b233d616379669987
Void Defi <152207837+VoidDefi@users.noreply.github.com>
2024-12-24 23:36:15 +0200

    ·`✶Fix fix in ExceptionLogger✶´·

--- ConsoleAdventure/Content/Scripts/Settings/ExceptionLogger.cs
+++ ConsoleAdventure/Content/Scripts/Settings/ExceptionLogger.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace ConsoleAdventure.Content.Scripts.Settings
@@ -31,8 +31,6 @@ namespace ConsoleAdventure.Content.Scripts.Settings
 
         public void AddText(string text)
         {
-            if (IsFileLocked(logFilePath)) return;
-
             TryLogExist();
             File.AppendAllText(logFilePath, text);
             Console.WriteLine(text);
@@ -66,22 +64,6 @@ namespace ConsoleAdventure.Content.Scripts.Settings
             {
                 File.Create(logFilePath).Dispose();
             }
-        }
-
-        static bool IsFileLocked(string filePath)
-        {
-            try
-            {
-                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
-                {
-                }
-            }
-            catch (IOException)
-            {
-                // Исключение указывает, что файл используется
-                return true;
-            }
-            return false;
         }
     }
 }
